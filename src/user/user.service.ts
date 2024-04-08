3149048b02121de34f45f4b994243a57706baf7e
msirajkarim <m.siraj@cofeapp.com>
2024-04-08 16:26:14 +0500

    initial commit

---
+++ src/user/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRepository } from './repo/user.repository';
+import { User } from './entities/user.entity';
+import { Constants } from 'src/utils/constants';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>) {}
+
+  create(createUserDto: CreateUserDto) {
+    let user: User = new User();
+    user.email = createUserDto.email;
+    user.firstName = createUserDto.firstName;
+    user.lastName = createUserDto.lastName;
+    user.password = createUserDto.password;
+    user.role = Constants.ROLES.NORMAL_ROLE;
+    return this.userRepository.save(user);
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findUserByEmail(email: string) {
+    return this.userRepository.findOneOrFail({where: {email:email}});
+  }
+
+  remove(id: number) {
+    return this.userRepository.delete(+id);
+  }
+}
